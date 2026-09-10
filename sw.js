5923738dee6404d1d17b5512967eafb167316de1
not-wp <54568753+not-wp@users.noreply.github.com>
2026-09-10 17:06:26 -0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,31 @@
+const CACHE = 'who-zscore-v1';
+const ASSETS = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
+
+self.addEventListener('install', function(e){
+  e.waitUntil(
+    caches.open(CACHE).then(function(cache){ return cache.addAll(ASSETS); })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', function(e){
+  e.waitUntil(
+    caches.keys().then(function(keys){
+      return Promise.all(keys.filter(function(k){ return k !== CACHE; }).map(function(k){ return caches.delete(k); }));
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', function(e){
+  e.respondWith(
+    caches.match(e.request).then(function(cached){
+      return cached || fetch(e.request).then(function(res){
+        return caches.open(CACHE).then(function(cache){
+          cache.put(e.request, res.clone());
+          return res;
+        });
+      }).catch(function(){ return caches.match('./index.html'); });
+    })
+  );
+});
