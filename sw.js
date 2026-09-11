9ed175c8ad7ad24eaf4b07bc3514203ffaecf598
not-wp <54568753+not-wp@users.noreply.github.com>
2026-09-10 23:09:32 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'who-zscore-v7';
+const CACHE = 'who-zscore-v9';
 const ASSETS = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', function(e){
